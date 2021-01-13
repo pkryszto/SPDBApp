96e4d4c9264d921368e6f3fe34426bfa2f6e219d
wwolny <wwolny1@gmail.com>
2021-01-13 18:33:55 +0100

    fix search location capitalize

--- src/main/java/QueryExecuter.java
+++ src/main/java/QueryExecuter.java
@@ -333,10 +333,10 @@ public class QueryExecuter {
     }
 
     public ArrayList<Address> findAddresses(String addressName) throws SQLException {
-
+        String capitAddressName = capitalize(addressName);
         PreparedStatement stmt = locationConnection.prepareStatement("""
                 select name, ST_AsText(way) as point from cities
-                where name like '""" +addressName+ """
+                where name like '""" +capitAddressName+ """
                 ' order by population asc
                 limit 10""");
 
@@ -360,4 +360,23 @@ public class QueryExecuter {
         return addresses;
     }
 
+    private static String capitalize(String str) {
+        if (str == null || str.isEmpty())
+            return "";
+        if (str.length() == 1)
+            return str.toUpperCase();
+        String[] parts = str.split(" ");
+        StringBuilder sb = new StringBuilder(str.length());
+
+        for (String part : parts) {
+            if (part.length() > 1)
+                sb.append(part.substring(0, 1).toUpperCase())
+                        .append(part.substring(1).toLowerCase());
+            else
+                sb.append(part.toUpperCase());
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
 }
